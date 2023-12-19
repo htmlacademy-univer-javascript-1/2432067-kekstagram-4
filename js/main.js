0aa14c7010efdb52d3c490cbe5a877fcacc90ade
hothalfbloodprince <esmola333@gmail.com>
2023-12-20 04:01:41 +0500

    g

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@ import {photos} from './data.js';
 import {renderPhotos} from './pictures.js';
 import { initForm } from './form.js';
 import './hashtags-pristine.js';
+import './effects.js';
 renderPhotos(photos);
 initForm();
