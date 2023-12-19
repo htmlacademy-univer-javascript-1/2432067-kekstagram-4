cb592a7b037ad532b04778ba81841f54584dc029
Keks <keks@htmlacademy.ru>
2023-12-19 13:42:07 +0300

    :heavy_check_mark: Сборка #12

--- 12/js/pictures.js
+++ 12/js/pictures.js
@@ -4,13 +4,14 @@ const pictures = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
 const renderPhoto = (picture) => {
-  const {url, description, likes, comments} = picture;
+  const {url, description, comments, likes} = picture;
   const pictureElement = pictureTemplate.cloneNode(true);
 
   pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__img').alt = description;
-  pictureElement.querySelector('.picture__likes').textContent = likes;
   pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+
 
   const onPictureElementClick = (event) => {
     event.preventDefault();
@@ -18,7 +19,7 @@ const renderPhoto = (picture) => {
     showBigPicture(picture);
   };
 
-  pictureElement.addElement('click', onPictureElementClick);
+  pictureElement.addEventListener('click', onPictureElementClick);
 
   return pictureElement;
 };
