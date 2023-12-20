1ac96b9c071a3b16f2ae360a0c7a35d4024e36ec
hothalfbloodprince <esmola333@gmail.com>
2023-12-20 12:32:45 +0500

    g

--- js/form.js
+++ js/form.js
@@ -74,10 +74,12 @@ const changeZoom = (factor = 1) => {
 
   if (size < Zoom.MIN) {
     size = Zoom.MIN;
+    return;
   }
 
   if (size > Zoom.MAX) {
     size = Zoom.MAX;
+    return;
   }
 
   scaleControlValue.value = `${size}%`;
