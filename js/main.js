ce5bf98b536ad7d6996db4120c59050cff6fe535
hothalfbloodprince <esmola333@gmail.com>
2023-12-20 05:14:34 +0500

    Надо подкачаться

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
-import { photos } from './data.js';
+import { loadData } from './fetch.js';
 import { renderPhotos } from './pictures.js';
 import { initForm } from './form.js';
 import './hashtags-pristine.js';
-renderPhotos(photos);
+import './messages.js';
 initForm();
+
+let photos = [];
+
+const onSuccess = (data) => {
+  photos = data.slice();
+  renderPhotos(photos);
+};
+
+const onError = () => {
+  const messageAlert = document.createElement('div');
+  messageAlert.style.position = 'absolute';
+  messageAlert.style.left = 0;
+  messageAlert.style.top = 0;
+  messageAlert.style.right = 0;
+  messageAlert.style.fontSize = '30px';
+  messageAlert.style.backgroundColor = 'red';
+  messageAlert.style.textAlign = 'center';
+  messageAlert.textContent = 'Ошибка загрузки фотографий';
+  document.body.append(messageAlert);
+};
+
+loadData(onSuccess, onError);
