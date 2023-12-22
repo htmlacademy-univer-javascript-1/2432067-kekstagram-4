8aa631c811311577ba5994ccc5e1221300d7e581
hothalfbloodprince <esmola333@gmail.com>
2023-12-22 20:11:49 +0500

    правки

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ import { openForm } from './form.js';
 import './hashtags-pristine.js';
 import './effects.js';
 import { loadData } from './fetch.js';
+import { showAlert } from './util.js';
 import './filters.js';
 import './add-photos.js';
 
@@ -16,17 +17,7 @@ const onSuccess = (data) => {
 };
 
 const onError = () => {
-  const messageAlert = document.createElement('div');
-  messageAlert.style.position = 'absolute';
-  messageAlert.style.left = 0;
-  messageAlert.style.top = 0;
-  messageAlert.style.right = 0;
-  messageAlert.style.fontSize = '20px';
-  messageAlert.style.backgroundColor = '#e1375f';
-  messageAlert.style.padding = '15px';
-  messageAlert.style.textAlign = 'center';
-  messageAlert.textContent = 'Ошибка загрузки фотографий';
-  document.body.append(messageAlert);
+  showAlert();
 };
 
 loadData(onSuccess, onError);
